ed03bd55f2860dbd3c75ac7a32b56067f318e372
Andre Mansour <amansour@AMANSOUR.local>
2013-08-12 19:18:42 -0700

    bac app

--- app.rb
+++ app.rb
@@ -3,11 +3,44 @@ require 'sinatra'
 require 'haml'
 require 'shotgun'
 require 'bundler/setup'
+require 'twilio-ruby'
 
 def round_to_precision(num, prec)
   num = num * (10 ** prec)
   num = num.round
   num = num.to_f / (10 ** prec).to_f
+end
+
+account_sid = "ACfa67ab7b63d3ae16a74365cd0cb14ae2"
+auth_token = "f79e1d47cc1cc36ae5de4ead98b226b2"
+client = Twilio::REST::Client.new account_sid, auth_token
+ 
+from = "+16506459938" # Your Twilio number
+ 
+friends = {
+"+16507038808" => "Andre",
+}
+friends.each do |key, value|
+  client.account.sms.messages.create(
+    :from => from,
+    :to => key,
+    :body => "Hey #{value}, Monkey party at 6PM. Bring Bananas!"
+  ) 
+  puts "Sent message to #{value}"
+end
+
+friends.each do |key, value|
+  client.account.sms.messages.create(
+  :from => from,
+  :to => key,
+  :body => "Hey Andre")
+end
+
+get '/' do
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Sms "Hey What is your weight? How long you been boozin? How much have ya had?!"
+  end
+  twiml.text
 end
 
 get '/' do
--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
-require 'bloodlacoholapp'
+require 'app'
 run Sinatra::Application
