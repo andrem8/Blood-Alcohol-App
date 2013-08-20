8dca11ea79ec2387ec7e6090d0b13fe8c8ea1b5a
Andre Mansour <amansour@AMANSOUR.local>
2013-08-20 14:13:35 -0700

    bac app

--- app.rb
+++ app.rb
@@ -72,7 +72,7 @@ get '/' do
   
       subliml = Twilio::TwiML::Response.new do |r| 
         if bac >= 0.08  
-          r.Sms "Your BAC of #{bac} is over 0.08.  It will be #{hoursleft} hours and #{minutesleft} minutes until you are under the limit. Text tweet and a message to anonymously exclaim your inebriation!"
+          r.Sms "Your BAC of #{bac} is over 0.08.  In #{hoursleft} hrs and #{minutesleft} mins you (may) be under the limit. Text tweet and a message!"
         elsif bac.between?(0,0.08)
           r.Sms "Your BAC of #{bac} is under the limit"
         elsif bac.between?(-0.5,0)
@@ -88,7 +88,7 @@ get '/' do
   
     subliml = Twilio::TwiML::Response.new do |r| 
       if bac >= 0.08  
-        r.Sms "Your BAC of #{bac} is over 0.08.  It will be #{hoursleft} hours and #{minutesleft} minutes until you are under the limit. Text tweet and a message to anonymously exclaim your inebriation!"
+        r.Sms "Your BAC of #{bac} is over 0.08.  In #{hoursleft} hrs and #{minutesleft} mins you (may) be under the limit. Text tweet and a message!"
       elsif bac.between?(0,0.08)
         r.Sms "Your BAC of #{bac} is under the limit"
       elsif bac.between?(-0.5,0)
