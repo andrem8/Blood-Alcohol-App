ee73136865e631ad2c1c0377de30d2eb22ba0a23
Andre Mansour <amansour@AMANSOUR.local>
2013-08-21 14:56:32 -0700

    bac app

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require 'rubygems'
+#require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'bundler/setup'
@@ -14,39 +14,106 @@ def round_to_precision(num, prec)
 end
 
 def handle_weight
-  weight = (params[:Body].partition(' ').last.to_f / 2.2) * 0.58
-  if weight == false
+  session[:weight] = (params[:Body].partition(' ').last.to_f / 2.2) * 0.58
+  if session[:weight].nil? == true
     puts "error"
-  elsif weight < 0
-    puts "error"
-    session[:weight]
   end
 end
 
 def handle_drinks
-  drinks = params[:Body].partition(' ').last.to_f * 0.9672
-  if drinks == false
+  session[:drinks] = params[:Body].partition(' ').last.to_f * 0.9672
+  if session[:drinks].nil? == true
     puts "error"
-  elsif weight < 0
-    puts "error"
-  elsif
-    session[:drinks]
   end
 end
 
-def handle_tweet
-  twitter = params[:Body].partition(' ').last
-  if twitter == false
+def handle_time
+  session[:time] = params[:Body].partition(' ').last.to_f * 0.015
+  if session[:time].nil? == true
     puts "error"
-  if twitter.include?("tweet")
-    session[:twitter]
-  else
-    puts "error"
-  end
   end
 end
 
+def handle_citylocate
+  session[:citylocate] = params[:FromCity]
+  if session[:citylocate].nil? == true
+    puts "citylocate error"
+  end
+end
 
+def handle_twitterstatus
+  session[:twitter] = params[:Body].partition(' ').last
+  if session[:twitter].nil? == true
+    puts "error"
+  end
+end
+
+def handle_bac
+  session[:bac] = round_to_precision(session[:drinks]/session[:weight]-session[:time],3)
+    if session[:bac].nil? == true
+      puts "error"
+    end
+end
+
+def handle_timeleft
+  session[:timeleft] = 40*(session[:bac]-0.08)/0.01
+  if session[:timeleft].nil? == true
+    puts "timeleft error"
+  end
+end
+
+def handle_hoursleft
+  session[:hoursleft] = (session[:timeleft] / 60).floor 
+  if session[:hoursleft].nil? == true
+    puts "hoursleft error"
+  end
+end
+
+def handle_minutesleft
+  session[:minutesleft] = (session[:timeleft] - (session[:hoursleft].floor * 60)).floor 
+  if session[:minutesleft].nil? == true
+    puts "minutesleft error"
+  end
+end
+
+def handle_bac_response
+  subliml = Twilio::TwiML::Response.new do |r| 
+    if session[:bac] == nil
+      puts "error terror"
+    else
+        if session[:bac] >= 0.08  
+          r.Sms "Your BAC of #{session[:bac]} is over 0.08.  In #{session[:hoursleft]} hrs #{session[:minutesleft]} mins you (may) be under the limit. Text tweet and a message!"
+        elsif session[:bac].between?(0,0.08)
+          r.Sms "Your BAC of #{session[:bac]} is under the limit"
+        elsif session[:bac].between?(-0.5,0)
+          r.Sms "Have another beer"
+        end
+    end
+  end
+end
+ 
+def handle_welcome_bac
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Sms "Hey there! Welcome to the BAC app! Please text weight followed by well your weight"
+  end
+end
+def handle_drinks_sms
+  himl = Twilio::TwiML::Response.new do |r|
+    r.Sms "Cool, now text drinks followed by how many drinks you've had"
+  end
+end
+def handle_time_sms
+  timl = Twilio::TwiML::Response.new do |r|
+    r.Sms "Almost there!!! Text time followed by how long have you been drinking"
+  end
+end
+def handle_error
+  swiml = Twilio::TwiML::Response.new do |r|
+    r.Sms "Hmm, try something else"
+  end
+end
+
+ 
 account_sid = "ACfa67ab7b63d3ae16a74365cd0cb14ae2"
 auth_token = "f79e1d47cc1cc36ae5de4ead98b226b2"
 client = Twilio::REST::Client.new account_sid, auth_token
@@ -66,12 +133,10 @@ path    = "/1.1/statuses/update.json"
 address = URI("#{baseurl}#{path}")
 request = Net::HTTP::Post.new address.request_uri
 
-
 http             = Net::HTTP.new address.host, address.port
 http.use_ssl     = true
 http.verify_mode = OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 http.start
-
 
 get '/' do
   
@@ -81,99 +146,30 @@ get '/' do
   else 
     @x = params[:Body].downcase
   end
-  if @x.include?("weight")
-    handle_weight
-  end
   
-    
-  if @x.nil? == true
-    puts "changed something!"
-  else
-    session[:a] = (params[:Body].partition(' ').last.to_f / 2.2) * 0.58 if @x.include?("weight")
-    session[:b] = params[:Body].partition(' ').last.to_f * 0.9672 if @x.include?("drinks")
-    session[:c] = params[:Body].partition(' ').last.to_f * 0.015 if @x.include?("time")
-    
-    citylocate = params[:FromCity]
-    if session[:a].nil? == true && @x.include?("drunk") == true
-      twiml = Twilio::TwiML::Response.new do |r|
-          r.Sms "Hey there! Welcome to the BAC app! Please text weight followed by well your weight"
-        end
-      twiml.text
-    elsif session[:b].nil? == true && @x.include?("weight") == true
-      himl = Twilio::TwiML::Response.new do |r|
-        r.Sms "Cool, now text drinks followed by how many drinks you've had"
-        end
-      himl.text
-    elsif session[:c].nil? == true && @x.include?("drinks") == true
-      timl = Twilio::TwiML::Response.new do |r|
-        r.Sms "Almost there!!! Text time followed by how long have you been drinking"
-        end
-      timl.text
-    elsif @x.include?("time") == true
-      bac = round_to_precision(session[:b]/session[:a]-session[:c],3)
-      timeleft = 40*(bac-0.08)/0.01 
-      hoursleft = (timeleft / 60).floor 
-      minutesleft = (timeleft - (hoursleft.floor * 60)).floor 
+   
+  if @x.include?("drunk") then
+     handle_welcome_bac.text
+  elsif @x.include?("weight")
+     handle_weight
+     handle_drinks_sms.text
+  elsif @x.include?("drinks")
+     handle_drinks
+     handle_time_sms.text
+  elsif @x.include?("time")
+     handle_time
+     handle_bac
+     handle_timeleft
+     handle_hoursleft
+     handle_minutesleft
+     handle_bac_response.text
+   elsif @x.include?("tweet")
+     handle_citylocate
+     handle_twitterstatus
+     request.set_form_data(
+       "status" => "#{session[:twitter]}\##{session[:citylocate]}  \##{session[:bac]}")
+     request.oauth! http, consumer_key, access_token
+     response = http.request request
+   end
+ end
   
-      subliml = Twilio::TwiML::Response.new do |r| 
-        if bac >= 0.08  
-          r.Sms "Your BAC of #{round_to_precision(bac, 2)} is over 0.08.  In #{hoursleft} hrs #{minutesleft} mins you (may) be under the limit. Text tweet and a message!"
-        elsif bac.between?(0,0.08)
-          r.Sms "Your BAC of #{round_to_precision(bac, 2)} is under the limit"
-        elsif bac.between?(-0.5,0)
-          r.Sms "Have another beer"
-        end
-      end
-      subliml.text
-    elsif @x.include?("twitter")
-      request.set_form_data(
-        "status" => "\##{citylocate} \##{round_to_precision(bac, 2)}")
-      request.oauth! http, consumer_key, access_token
-      response = http.request request
-    else 
-    
-    bac = round_to_precision(session[:b]/session[:a]-session[:c],3)
-    timeleft = 40*(bac-0.08)/0.01 
-    hoursleft = (timeleft / 60).floor 
-    minutesleft = (timeleft - (hoursleft.floor * 60)).floor 
-  
-    subliml = Twilio::TwiML::Response.new do |r| 
-      if bac >= 0.08  
-        r.Sms "Your BAC of #{round_to_precision(bac, 2)} is over 0.08.  In #{hoursleft} hrs #{minutesleft} mins you (may) be under the limit. Text tweet and a message!"
-      elsif bac.between?(0,0.08)
-        r.Sms "Your BAC of #{round_to_precision(bac, 2)} is under the limit"
-      elsif bac.between?(-0.5,0)
-        r.Sms "Have another beer"
-      end
-    end
-
-    twiml = Twilio::TwiML::Response.new do |r|
-        r.Sms "Hey there! Welcome to the BAC app! Please text weight followed by well your weight"
-      end
-    himl = Twilio::TwiML::Response.new do |r|
-      r.Sms "Cool, now text drinks followed by how many drinks you've had"
-      end
-    timl = Twilio::TwiML::Response.new do |r|
-      r.Sms "Almost there!!! Text time followed by how long have you been drinking"
-      end
-    swiml = Twilio::TwiML::Response.new do |r|
-      r.Sms "Hmm, try something else"
-    end
-    
-     if @x.include?("drunk") then
-        twiml.text 
-     elsif @x.include?("weight")
-        himl.text
-     elsif @x.include?("drinks")
-        timl.text
-     elsif @x.include?("time")
-        subliml.text
-     elsif @x.include?("tweet")
-       request.set_form_data(
-         "status" => "\##{citylocate} \##{round_to_precision(bac, 2)}")
-       request.oauth! http, consumer_key, access_token
-       response = http.request request
-      end
-    end
-  end
-end
